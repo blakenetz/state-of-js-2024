67f9b255bcf8b4b8bad238853feda6550aa50ade
Blake Netzeband <blake.netzeband@gmail.com>
2025-01-09 09:40:22 -0700

    expand on example

--- packages/features/index.ts
+++ packages/features/index.ts
@@ -1,8 +1,17 @@
-import { value } from "./data/liveBindings";
+import { value as staticValue } from "./data/liveBindings";
+
+const time = 1500;
 
 async function main() {
-  console.log("Initial value", value);
-  setTimeout(() => console.log("After 1000ms", value), 1000);
+  const { value: dynamicValue } = await import("./data/liveBindings");
+
+  console.log("[STATIC] initial value", staticValue);
+  console.log("[DYNAMIC] initial value", dynamicValue);
+
+  setTimeout(() => {
+    console.log(`[STATIC] after ${time}ms`, staticValue);
+    console.log(`[DYNAMIC] after ${time}ms`, dynamicValue);
+  }, time);
 }
 
 main().catch((e) => console.error("Error in main.ts", e));
