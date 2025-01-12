69c10dc70a7030625a642027708fdf5992522b00
Blake Netzeband <blake.netzeband@gmail.com>
2025-01-12 10:17:44 -0700

    code cleanup

--- packages/features/server/index.ts
+++ packages/features/server/index.ts
@@ -1,9 +1,7 @@
 import http from "node:http";
 import fs from "node:fs";
 import handleWebSocket from "./webSocket";
-
-export const port = 3000;
-export const host = "localhost";
+import { host, port } from "../util";
 
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
@@ -11,26 +9,32 @@ const server = http.createServer((req, res) => {
   const readOptions: { encoding: BufferEncoding } = { encoding: "utf-8" };
   switch (req.url) {
     case "/":
-      res.writeHead(200, { "Content-Type": "text/html" });
-      const html = fs.readFileSync("client/index.html", readOptions);
-      res.end(html);
+      try {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        const html = fs.readFileSync("client/index.html", readOptions);
+        res.end(html);
+      } catch (error) {}
       break;
 
     case "/module":
-      res.writeHead(200, { "Content-Type": "text/javascript" });
-      const js = fs.readFileSync("data/liveBindings.ts", readOptions);
-      res.end(js);
+      try {
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        const js = fs.readFileSync("data/liveBindings.ts", readOptions);
+        res.end(js);
+      } catch (error) {}
       break;
 
     case "/data":
-      res.writeHead(200, { "Content-Type": "application/json" });
-      const json = fs.readFileSync("data/data.json", readOptions);
-      res.end(json);
+      try {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        const json = fs.readFileSync("data/data.json", readOptions);
+        res.end(json);
+      } catch (error) {}
       break;
 
     default:
       res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end('Only GET methods on "/" endpoint is available!');
+      res.end("NOT FOUND");
       break;
   }
 });
